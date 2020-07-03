7cff8a23ef4586fbe0f2cd1ab13a83d44ef079a5
Joao <joao_gens@hotmail.com>
2020-07-03 16:37:23 +0100

    edit and delete products

--- demo/src/main/java/com/morethenonecoffe/demo/controller/ProductController.java
+++ demo/src/main/java/com/morethenonecoffe/demo/controller/ProductController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -39,5 +41,22 @@ public class ProductController {
         service.save(product);
 
         return "redirect:/";
+    }
+
+    @RequestMapping("/edit/{id}")
+    public ModelAndView editProduct(@PathVariable(name="id") long id) {
+
+        ModelAndView mav = new ModelAndView("edit_product");
+        Product product = service.get(id);
+        mav.addObject("product", product);
+        return mav;
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable(name="id") long id) {
+
+       service.delete(id);
+
+       return "redirect:/";
     }
 }
