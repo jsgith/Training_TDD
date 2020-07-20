c9618886b21b82a1ce145190094662b0efd0ea5f
Joao <joao_gens@hotmail.com>
2020-07-20 14:52:22 +0100

    init maisdoqueumcafeApp

---
+++ maisdoqueumcafe/src/main/java/com/maisdoqueumcafe/maisdoqueumcafe/MaisdoqueumcafeApplication.java
@@ -0,0 +1,13 @@
+package com.maisdoqueumcafe.maisdoqueumcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisdoqueumcafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaisdoqueumcafeApplication.class, args);
+	}
+
+}
