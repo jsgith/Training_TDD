408795f8bdca959b9494f59ad12f0d764680432f
Joao <joao_gens@hotmail.com>
2020-08-05 17:48:45 +0100

    test bad request in signup fields

--- maisdoqueumcafe/src/main/java/com/maisdoqueumcafe/maisdoqueumcafe/user/User.java
+++ maisdoqueumcafe/src/main/java/com/maisdoqueumcafe/maisdoqueumcafe/user/User.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data // lombok automate constructors, getters and setters
 @Entity // For mapping an object to a database
@@ -13,7 +16,17 @@ public class User {
     @Id //Tell JPA that this field will be the primary key
     @GeneratedValue // Tell JPA how this Id will be generated. The default one is AUTO. Creates unic Ids for us
     private long id;
+
+    @NotNull
+    @Size(min = 4, max=255)
     private String username;
+
+    @NotNull
+    @Size(min = 4, max= 255)
     private String displayName;
+
+    @NotNull
+    @Size(min = 8, max=255)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
     private String password;
 }
