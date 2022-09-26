1f3f7ef2d2fd6f682ed6ac4377adf030a55f6b1e
Joao <joao_gens@hotmail.com>
2022-09-26 14:42:27 +0100

    dealing with validation messagesand the different languagues

--- UpForTDD/backend/src/main/java/com/upfor/upfor/user/User.java
+++ UpForTDD/backend/src/main/java/com/upfor/upfor/user/User.java
@@ -20,7 +20,7 @@ public class User {
     @GeneratedValue
     private long id;
 
-    @NotNull
+    @NotNull(message = "{upfor.constraints.username.NotNull.message}")
     @Size(min = 4, max = 255)
     private String username;
 
@@ -30,7 +30,7 @@ public class User {
 
     @NotNull
     @Size(min = 8, max = 255)
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message= "{upfor.constraints.password.Pattern.message}")
     private String password;
     
 }
